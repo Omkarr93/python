5cc7a7b122509de88d2c162bb10e7706e0556680
omkar <bomkar32@gmail.com>
2022-01-27 19:58:45 +0530

    Pandas

--- Pandas/file1.py
+++ Pandas/file1.py
@@ -1,2 +1,19 @@
 import numpy as np
-impo
+import pandas as pd
+
+dict1 = {
+    "name": ['Omkar', 'Raj', 'Neha', 'Pratik'],
+    "Marks": [91, 52, 42, 32],
+    "city": ['kolhapur', 'Pune', 'Pune', 'Mumbai']
+}
+
+# df = pd.DataFrame(dict1)
+#
+# print(df)
+# df.to_csv('testcsv_file.csv')
+# df.head(2)
+# df.to_csv('testcsv_file_NOindex.csv',index = False)
+
+omkar = pd.read_csv('Omkar1.csv')
+
+print(omkar)
---
+++ ROW_file/01_file.py
@@ -0,0 +1,14 @@
+import sys
+
+
+def convert(string):
+    li = string.split("\n")
+    return li
+def my_function ()
+
+
+
+f = open("file.txt")
+content = f.read()
+print(convert(content))
+f.close()
